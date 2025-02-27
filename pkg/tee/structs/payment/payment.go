762613b87c68fc23906a20194ba46d8066b2ce8e
grePod <gregor.podlogar@flare.network>
2025-02-27 16:25:02 +0100

    decoders of messages in instructions

--- pkg/tee/structs/payment/payment.go
+++ pkg/tee/structs/payment/payment.go
@@ -1,2 +1,47 @@
 //go:generate  abigen --abi=pkg/tee/structs/payment/payment.abi --pkg=payment --type=Payment --out=pkg/tee/structs/payment/autogen.go
 package payment
+
+import (
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/flare-foundation/go-flare-common/pkg/logger"
+)
+
+type OPCommand string
+
+const (
+	Pay     OPCommand = "PAY"
+	Reissue OPCommand = "REISSUE"
+)
+
+var opCommands = []OPCommand{
+	Pay,
+	Reissue,
+}
+
+// i-th method correspond to a method in TeeRegistryStruct interface whose
+// input is the type of message emitted with i-th opCommands
+var methods = []string{
+	"paymentInstructionMessageStruct",
+	"paymentInstructionMessageStruct",
+}
+
+var MessageArguments map[OPCommand]abi.Argument
+
+func init() {
+	paymentAbi, err := PaymentMetaData.GetAbi()
+	if err != nil {
+		logger.Panicf("error getting registry abi: %v", err)
+	}
+
+	if len(methods) != len(opCommands) {
+		logger.Panicf("methods, opCommands miss match")
+	}
+
+	for j := range opCommands {
+		method, ok := paymentAbi.Methods[methods[j]]
+		if !ok {
+			logger.Panicf("missing method %s", methods[j])
+		}
+		MessageArguments[opCommands[j]] = method.Inputs[0]
+	}
+}
--- pkg/tee/structs/registry/registry.go
+++ pkg/tee/structs/registry/registry.go
@@ -1,2 +1,52 @@
 //go:generate  abigen --abi=pkg/tee/structs/registry/registry.abi --pkg=registry --type=Registry --out=pkg/tee/structs/registry/autogen.go
 package registry
+
+import (
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/flare-foundation/go-flare-common/pkg/logger"
+)
+
+const OPType = "REG"
+
+type OPCommand string
+
+const (
+	AvailabilityCheck OPCommand = "AVAILABILITY_CHECK"
+	ToPauseForUpgrade OPCommand = "TO_PAUSE_FOR_UPGRADE"
+	ReplicateFrom     OPCommand = "REPLICATE_FROM"
+)
+
+var opCommands = []OPCommand{
+	AvailabilityCheck,
+	ToPauseForUpgrade,
+	ReplicateFrom,
+}
+
+// i-th method correspond to a method in TeeRegistryStruct interface whose
+// input is the type of message emitted with i-th opCommands
+var methods = []string{
+	"availabilityCheckResponseStruct",
+	"pauseForUpgradeStruct",
+	"replicateTeeMachineStruct",
+}
+
+var MessageArguments map[OPCommand]abi.Argument
+
+func init() {
+	registryAbi, err := RegistryMetaData.GetAbi()
+	if err != nil {
+		logger.Panicf("error getting registry abi: %v", err)
+	}
+
+	if len(methods) != len(opCommands) {
+		logger.Panicf("methods, opCommands miss match")
+	}
+
+	for j := range opCommands {
+		method, ok := registryAbi.Methods[methods[j]]
+		if !ok {
+			logger.Panicf("missing method %s", methods[j])
+		}
+		MessageArguments[opCommands[j]] = method.Inputs[0]
+	}
+}
