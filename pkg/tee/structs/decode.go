355af3bcde7c0c2a0746e89d83e45e9443488005
grePod <gregor.podlogar@flare.network>
2025-02-27 13:21:00 +0100

    alternative decode + tests

--- pkg/tee/structs/decode.go
+++ pkg/tee/structs/decode.go
@@ -7,10 +7,10 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi"
 )
 
-// Decode decodes abi encoded data and writes it to destination.
+// DecodeTo decodes abi encoded data and writes it to destination.
 //
 // dest has to be a pointer to a struct that is structured as arg describes.
-func Decode(arg abi.Argument, data []byte, dest any) error {
+func DecodeTo[T any](arg abi.Argument, data []byte, dest *T) error {
 	rv := reflect.ValueOf(dest)
 	if rv.Kind() != reflect.Pointer || rv.IsNil() {
 		return fmt.Errorf("dest is not a non nil pointer")
@@ -37,4 +37,41 @@ func Decode(arg abi.Argument, data []byte, dest any) error {
 	}
 
 	return nil
+}
+
+// DecodeTo2 decodes abi encoded data and writes it to destination.
+//
+// dest has to be a pointer to a struct that is structured as arg describes.
+func DecodeTo2[T any](arg abi.Argument, data []byte, dest *T) error {
+	rv := reflect.ValueOf(dest)
+	if rv.Kind() != reflect.Pointer || rv.IsNil() {
+		return fmt.Errorf("dest is not a non nil pointer")
+	}
+
+	args := abi.Arguments{arg}
+
+	decodedSlice, err := args.Unpack(data)
+	if err != nil {
+		return err
+	}
+
+	*dest = *abi.ConvertType(decodedSlice[0], new(T)).(*T)
+	return nil
+}
+
+// DecodeTo decodes abi encoded data and writes it to destination.
+//
+// dest has to be a pointer to a struct that is structured as arg describes.
+func Decode[T any](arg abi.Argument, data []byte) (T, error) {
+	var t T
+	args := abi.Arguments{arg}
+
+	decodedSlice, err := args.Unpack(data)
+	if err != nil {
+		return t, err
+	}
+
+	t = *abi.ConvertType(decodedSlice[0], new(T)).(*T)
+
+	return t, nil
 }
