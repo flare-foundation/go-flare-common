c9a7aa7f8cdf5a881a274bd10077934ea808d372
grePod <gregor.podlogar@flare.network>
2025-03-06 16:55:55 +0100

    toml moved + test

--- pkg/toml/toml.go
+++ pkg/toml/toml.go
@@ -3,12 +3,13 @@ package toml
 import (
 	"fmt"
 	"os"
+	"reflect"
 
 	"github.com/naoina/toml"
 )
 
 // ReadToml reads toml file from filePath and marshals it into struct of type T.
-func ReadToml[T any](filePath string) (T, error) {
+func ReadToml[T any](filePath string, allowUnknownFields bool) (T, error) {
 	var config T
 
 	file, err := os.ReadFile(filePath)
@@ -16,7 +17,13 @@ func ReadToml[T any](filePath string) (T, error) {
 		return config, fmt.Errorf("failed reading file %s with: %s", filePath, err)
 	}
 
-	err = toml.Unmarshal(file, &config)
+	tomlCfg := toml.DefaultConfig
+
+	if allowUnknownFields {
+		tomlCfg.MissingField = func(typ reflect.Type, key string) error { return nil }
+	}
+
+	err = tomlCfg.Unmarshal(file, &config)
 	if err != nil {
 		return config, fmt.Errorf("failed unmarshaling file %s with: %s", filePath, err)
 	}
