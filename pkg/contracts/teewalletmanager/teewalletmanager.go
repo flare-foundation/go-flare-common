4a1cf1eb9f212e8048cbeab90f4f7d73c055c7c5
grePod <gregor.podlogar@flare.network>
2025-03-10 13:42:36 +0100

    contract bindings added

---
+++ pkg/contracts/teewalletmanager/teewalletmanager.go
@@ -0,0 +1,2 @@
+//go:generate  abigen --abi=pkg/contracts/teewalletmanager/teewalletmanager.abi --pkg=teewalletmanager --type=TeeWalletManager --out=pkg/contracts/teewalletmanager/autogen.go
+package teewalletmanager
