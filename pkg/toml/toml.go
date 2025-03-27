e256588a6508f520737262401c76731c4c893fae
grePod <gregor.podlogar@flare.network>
2025-03-27 16:21:31 +0100

    change toml decoding dependency

--- pkg/toml/toml.go
+++ pkg/toml/toml.go
@@ -3,9 +3,8 @@ package toml
 import (
 	"fmt"
 	"os"
-	"reflect"
 
-	"github.com/naoina/toml"
+	"github.com/pelletier/go-toml/v2"
 )
 
 // ReadToml reads toml file from filePath and marshals it into struct of type T.
@@ -13,42 +12,45 @@ import (
 func ReadToml[T any](filePath string, allowUnknownFields bool) (T, error) {
 	var dest T
 
-	file, err := os.ReadFile(filePath)
+	file, err := os.Open(filePath)
 	if err != nil {
 		return dest, fmt.Errorf("failed reading file %s with: %s", filePath, err)
 	}
 
-	tomlCfg := toml.DefaultConfig
+	dec := toml.NewDecoder(file)
 
-	if allowUnknownFields {
-		tomlCfg.MissingField = func(typ reflect.Type, key string) error { return nil }
+	if !allowUnknownFields {
+		dec = dec.DisallowUnknownFields()
 	}
 
-	err = tomlCfg.Unmarshal(file, &dest)
+	err = dec.Decode(&dest)
 	if err != nil {
 		return dest, fmt.Errorf("failed unmarshaling file %s with: %s", filePath, err)
 	}
 
-	return dest, nil
+	err = file.Close()
+	return dest, err
 }
 
 // ReadTomlTo reads toml file from filePath and marshals it into dest.
 // If allowUnknownFields is set to false, any unknown field in toml file will trigger error.
 func ReadTomlTo[T any](filePath string, dest *T, allowUnknownFields bool) error {
-	file, err := os.ReadFile(filePath)
+	file, err := os.Open(filePath)
 	if err != nil {
 		return fmt.Errorf("failed reading file %s with: %s", filePath, err)
 	}
 
-	tomlCfg := toml.DefaultConfig
-	if allowUnknownFields {
-		tomlCfg.MissingField = func(typ reflect.Type, key string) error { return nil }
+	dec := toml.NewDecoder(file)
+
+	if !allowUnknownFields {
+		dec = dec.DisallowUnknownFields()
 	}
 
-	err = tomlCfg.Unmarshal(file, dest)
+	err = dec.Decode(&dest)
 	if err != nil {
 		return fmt.Errorf("failed unmarshaling file %s with: %s", filePath, err)
 	}
 
-	return nil
+	err = file.Close()
+	return err
 }
