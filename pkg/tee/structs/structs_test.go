229da4d83be68f33794e3d09417ceb5a0ba54e38
grePod <gregor.podlogar@flare.network>
2025-04-18 15:21:14 +0200

    xrpl vector256 serialization added

---
+++ pkg/tee/structs/structs_test.go
@@ -0,0 +1,20 @@
+package structs
+
+import (
+	"testing"
+
+	"github.com/flare-foundation/go-flare-common/pkg/tee/structs/connector"
+	"github.com/flare-foundation/go-flare-common/pkg/tee/structs/payment"
+	"github.com/flare-foundation/go-flare-common/pkg/tee/structs/registry"
+	"github.com/flare-foundation/go-flare-common/pkg/tee/structs/wallet"
+	"github.com/stretchr/testify/require"
+)
+
+// This test assures that all inits in structs directory are called and do not panic.
+func TestInit(t *testing.T) {
+	require.NotNil(t, payment.MessageArguments)
+	require.NotNil(t, registry.MessageArguments)
+	require.NotNil(t, wallet.MessageArguments)
+	require.NotNil(t, connector.MessageArguments)
+	require.NotNil(t, connector.AttestationTypeArguments)
+}
