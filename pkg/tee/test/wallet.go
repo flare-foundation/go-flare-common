239a090b5bd3f57e5ed24a4939d93a9205916940
grePod <gregor.podlogar@flare.network>
2025-06-16 13:02:37 +0200

    tee registry update

--- pkg/tee/test/wallet.go
+++ pkg/tee/test/wallet.go
@@ -3,12 +3,9 @@ package test
 import (
 	"context"
 	"crypto/ecdsa"
-	"fmt"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/flare-foundation/go-flare-common/pkg/contracts/teeregistry"
 	"github.com/flare-foundation/go-flare-common/pkg/contracts/teewalletkeymanager"
@@ -63,108 +60,108 @@ func NewClient(prv *ecdsa.PrivateKey, chainClient *ethclient.Client, teeRegistry
 	}, nil
 }
 
-func (c *Client) InitializeWallet(opType common.Hash, submitAddress common.Address) (common.Hash, common.Hash, error) {
-	opts, err := bind.NewKeyedTransactorWithChainID(c.prv, c.chainID)
-	if err != nil {
-		return common.Hash{}, common.Hash{}, err
-	}
+// func (c *Client) InitializeWallet(opType common.Hash, submitAddress common.Address) (common.Hash, common.Hash, error) {
+// 	opts, err := bind.NewKeyedTransactorWithChainID(c.prv, c.chainID)
+// 	if err != nil {
+// 		return common.Hash{}, common.Hash{}, err
+// 	}
 
-	tx, err := c.teeWalletProjectManager.CreateProject(opts, opType, submitAddress)
-	if err != nil {
-		return common.Hash{}, common.Hash{}, err
-	}
+// 	tx, err := c.teeWalletProjectManager.CreateProject(opts, opType, submitAddress)
+// 	if err != nil {
+// 		return common.Hash{}, common.Hash{}, err
+// 	}
 
-	ctx := context.Background()
-	receipt, err := bind.WaitMined(ctx, c.chainClient, tx)
-	if err != nil {
-		return common.Hash{}, common.Hash{}, err
-	}
-	if receipt.Status != types.ReceiptStatusSuccessful {
-		return common.Hash{}, common.Hash{}, fmt.Errorf("transaction CreateProject %s not successful", tx.Hash())
-	}
+// 	ctx := context.Background()
+// 	receipt, err := bind.WaitMined(ctx, c.chainClient, tx)
+// 	if err != nil {
+// 		return common.Hash{}, common.Hash{}, err
+// 	}
+// 	if receipt.Status != types.ReceiptStatusSuccessful {
+// 		return common.Hash{}, common.Hash{}, fmt.Errorf("transaction CreateProject %s not successful", tx.Hash())
+// 	}
 
-	logProject, err := c.teeWalletProjectManager.ParseProjectCreated(*receipt.Logs[0])
-	if err != nil {
-		return common.Hash{}, common.Hash{}, err
-	}
-	projectID := logProject.ProjectId
+// 	logProject, err := c.teeWalletProjectManager.ParseProjectCreated(*receipt.Logs[0])
+// 	if err != nil {
+// 		return common.Hash{}, common.Hash{}, err
+// 	}
+// 	projectID := logProject.ProjectId
 
-	tx, err = c.teeWalletManager.CreateWallet(opts, projectID)
-	if err != nil {
-		return common.Hash{}, common.Hash{}, err
-	}
+// 	tx, err = c.teeWalletManager.CreateWallet(opts, projectID)
+// 	if err != nil {
+// 		return common.Hash{}, common.Hash{}, err
+// 	}
 
-	receipt, err = bind.WaitMined(ctx, c.chainClient, tx)
-	if err != nil {
-		return common.Hash{}, common.Hash{}, err
-	}
-	if receipt.Status != types.ReceiptStatusSuccessful {
-		return common.Hash{}, common.Hash{}, fmt.Errorf("transaction CreateWallet %s not successful", tx.Hash())
-	}
+// 	receipt, err = bind.WaitMined(ctx, c.chainClient, tx)
+// 	if err != nil {
+// 		return common.Hash{}, common.Hash{}, err
+// 	}
+// 	if receipt.Status != types.ReceiptStatusSuccessful {
+// 		return common.Hash{}, common.Hash{}, fmt.Errorf("transaction CreateWallet %s not successful", tx.Hash())
+// 	}
 
-	logWallet, err := c.teeWalletManager.ParseWalletCreated(*receipt.Logs[0])
-	if err != nil {
-		return common.Hash{}, common.Hash{}, err
-	}
-	walletID := logWallet.WalletId
+// 	logWallet, err := c.teeWalletManager.ParseWalletCreated(*receipt.Logs[0])
+// 	if err != nil {
+// 		return common.Hash{}, common.Hash{}, err
+// 	}
+// 	walletID := logWallet.WalletId
 
-	return projectID, walletID, nil
-}
+// 	return projectID, walletID, nil
+// }
 
-func (c *Client) AddKey(teeID common.Address, walletID common.Hash) (uint64, error) {
-	opts, err := bind.NewKeyedTransactorWithChainID(c.prv, c.chainID)
-	if err != nil {
-		return 0, err
-	}
+// func (c *Client) AddKey(teeID common.Address, walletID common.Hash) (uint64, error) {
+// 	opts, err := bind.NewKeyedTransactorWithChainID(c.prv, c.chainID)
+// 	if err != nil {
+// 		return 0, err
+// 	}
 
-	tx, err := c.teeWalletKeyManager.AddKey(opts, teeID, walletID)
-	if err != nil {
-		return 0, err
-	}
+// 	tx, err := c.teeWalletKeyManager.AddKey(opts, teeID, walletID)
+// 	if err != nil {
+// 		return 0, err
+// 	}
 
-	ctx := context.Background()
+// 	ctx := context.Background()
 
-	receipt, err := bind.WaitMined(ctx, c.chainClient, tx)
-	if err != nil {
-		return 0, err
-	}
-	if receipt.Status != types.ReceiptStatusSuccessful {
-		return 0, fmt.Errorf("transaction AddKey %s not successful", tx.Hash())
-	}
+// 	receipt, err := bind.WaitMined(ctx, c.chainClient, tx)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+// 	if receipt.Status != types.ReceiptStatusSuccessful {
+// 		return 0, fmt.Errorf("transaction AddKey %s not successful", tx.Hash())
+// 	}
 
-	log, err := c.teeWalletKeyManager.ParseWalletKeyAdded(*receipt.Logs[0])
-	if err != nil {
-		return 0, err
-	}
+// 	log, err := c.teeWalletKeyManager.ParseWalletKeyAdded(*receipt.Logs[0])
+// 	if err != nil {
+// 		return 0, err
+// 	}
 
-	return log.KeyId, nil
-}
+// 	return log.KeyId, nil
+// }
 
-func (c *Client) InitializeAndAddKeys() (common.Hash, []uint64, error) {
-	x := [32]byte{}
-	copy(x[:], "XRP")
+// func (c *Client) InitializeAndAddKeys() (common.Hash, []uint64, error) {
+// 	x := [32]byte{}
+// 	copy(x[:], "XRP")
 
-	submitAddress := common.BytesToAddress([]byte("randomAddress")) // unused so far
+// 	submitAddress := common.BytesToAddress([]byte("randomAddress")) // unused so far
 
-	_, walletID, err := c.InitializeWallet(x, submitAddress)
-	if err != nil {
-		return common.Hash{}, nil, err
-	}
+// 	_, walletID, err := c.InitializeWallet(x, submitAddress)
+// 	if err != nil {
+// 		return common.Hash{}, nil, err
+// 	}
 
-	ids, err := c.teeRegistry.GetActiveTeeIds(&bind.CallOpts{})
-	if err != nil {
-		return common.Hash{}, nil, err
-	}
+// 	ids, err := c.teeRegistry.GetActiveTees(&bind.CallOpts{})
+// 	if err != nil {
+// 		return common.Hash{}, nil, err
+// 	}
 
-	keyIDs := make([]uint64, len(ids))
+// 	keyIDs := make([]uint64, len(ids))
 
-	for i := range ids {
-		keyID, err := c.AddKey(ids[i], walletID)
-		if err != nil {
-			return common.Hash{}, nil, err
-		}
-		keyIDs[i] = keyID
-	}
+// 	for i := range ids {
+// 		keyID, err := c.AddKey(ids[i], walletID)
+// 		if err != nil {
+// 			return common.Hash{}, nil, err
+// 		}
+// 		keyIDs[i] = keyID
+// 	}
 
-	return walletID, keyIDs, nil
-}
+// 	return walletID, keyIDs, nil
+// }
