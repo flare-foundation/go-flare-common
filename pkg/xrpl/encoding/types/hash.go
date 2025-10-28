340581d15dc98d677410e94ce82a3f4142177ea9
grePod <gregor.podlogar@flare.network>
2025-10-28 15:10:01 +0100

    small tidy up

--- pkg/xrpl/encoding/types/hash.go
+++ pkg/xrpl/encoding/types/hash.go
@@ -11,6 +11,21 @@ type hash struct {
 	length int
 }
 
+// Blob is used for serialization of Blob fields. https://xrpl.org/docs/references/protocol/binary-format#blob-fields
+var Blob = &hash{length: 0}
+
+// Hash128 is used for serialization of Hash128 fields. https://xrpl.org/docs/references/protocol/binary-format#hash-fields
+var Hash128 = &hash{length: 16}
+
+// Hash160 is used for serialization of Hash160 fields. https://xrpl.org/docs/references/protocol/binary-format#hash-fields
+var Hash160 = &hash{length: 20}
+
+// Hash192 is used for serialization of Hash192 fields. https://xrpl.org/docs/references/protocol/binary-format#hash-fields
+var Hash192 = &hash{length: 24}
+
+// Hash256 is used for serialization of Hash256 fields. https://xrpl.org/docs/references/protocol/binary-format#hash-fields
+var Hash256 = &hash{length: 32}
+
 // ToBytes serializes hexadecimal strings.
 func (h *hash) ToBytes(value any, _ bool) ([]byte, error) {
 	s, ok := value.(string)
@@ -52,18 +67,3 @@ func (h *hash) ToJSON(b *bytes.Buffer, length int) (any, error) {
 
 	return strings.ToUpper(hex.EncodeToString(value)), nil
 }
-
-// Blob is used for serialization of Blob fields. https://xrpl.org/docs/references/protocol/binary-format#blob-fields
-var Blob = &hash{length: 0}
-
-// Hash128 is used for serialization of Hash128 fields. https://xrpl.org/docs/references/protocol/binary-format#hash-fields
-var Hash128 = &hash{length: 16}
-
-// Hash160 is used for serialization of Hash160 fields. https://xrpl.org/docs/references/protocol/binary-format#hash-fields
-var Hash160 = &hash{length: 20}
-
-// Hash192 is used for serialization of Hash192 fields. https://xrpl.org/docs/references/protocol/binary-format#hash-fields
-var Hash192 = &hash{length: 24}
-
-// Hash256 is used for serialization of Hash256 fields. https://xrpl.org/docs/references/protocol/binary-format#hash-fields
-var Hash256 = &hash{length: 32}
