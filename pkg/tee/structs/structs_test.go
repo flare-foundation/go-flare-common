350b391e88553246222f8de9e8fd3407bc560522
grePod <gregor.podlogar@flare.network>
2025-10-29 16:49:56 +0100

    MachineRegistry TeeMachineData struct args added

--- pkg/tee/structs/structs_test.go
+++ pkg/tee/structs/structs_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/flare-foundation/go-flare-common/pkg/tee/structs/connector"
+	"github.com/flare-foundation/go-flare-common/pkg/tee/structs/machineregistry"
 	"github.com/flare-foundation/go-flare-common/pkg/tee/structs/payment"
 	"github.com/flare-foundation/go-flare-common/pkg/tee/structs/tee"
 	"github.com/flare-foundation/go-flare-common/pkg/tee/structs/wallet"
@@ -18,4 +19,5 @@ func TestInit(t *testing.T) {
 	require.NotNil(t, connector.MessageArguments)
 	require.NotNil(t, connector.AttestationTypeArguments)
 	require.NotNil(t, tee.StructArg)
+	require.NotNil(t, machineregistry.TeeMachineDataStructArg)
 }
