5baa57c4db206dac30d5809c3e590945e58a0370
Urška Krivc <urska.krivc@abelium.com>
2025-12-18 08:13:07 +0100

    add contracts: teefeecalculator and ftdcrequestfeeconfigurations, update abi_update.sh

---
+++ pkg/contracts/ftdcrequestfeeconfigurations/ftdcrequestfeeconfigurations.go
@@ -0,0 +1,2 @@
+//go:generate  abigen --abi=ftdcrequestfeeconfigurations.abi --pkg=ftdcrequestfeeconfigurations --type=FtdcRequestFeeConfigurations --out=autogen.go
+package ftdcrequestfeeconfigurations
---
+++ pkg/contracts/teefeecalculator/teefeecalculator.go
@@ -0,0 +1,2 @@
+//go:generate  abigen --abi=teefeecalculator.abi --pkg=teefeecalculator --type=TeeFeeCalculator --out=autogen.go
+package teefeecalculator
