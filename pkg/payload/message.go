9ebb421afb84404b63d1a646abe55b70363a788e
grePod <gregor.podlogar@flare.network>
2025-12-22 14:52:06 +0100

    small refactor, remove magic numbers

--- pkg/payload/message.go
+++ pkg/payload/message.go
@@ -15,6 +15,12 @@ const (
 	Retry ResponseStatus = "RETRY"
 )
 
+const (
+	msgHeaderLength             = 7  // protocolID 1, votingRoundID 4, payloadLength 2
+	submitSignatureHeaderLength = 6  // protocolID 1, roundID 4, randomQualityScore 1
+	submitSignatureMsgLength    = 38 // protocolID 1, roundID 4, randomQualityScore 1, merkleRoot 32
+)
+
 type SubprotocolResponse struct {
 	Status         ResponseStatus `json:"status"`
 	Data           string         `json:"data"`
@@ -24,7 +30,8 @@ type SubprotocolResponse struct {
 // BuildMessage builds a submit message string from protocolID, votingRoundID and payload.
 // The message string is in the format: 0x<protocolID(1 byte)><votingRoundID(4 byte)><payloadLength(2 byte)><payload>.
 func BuildMessage(protocolID uint8, votingRoundID uint32, payload []byte) string {
-	message := make([]byte, 7)
+	message := make([]byte, msgHeaderLength, msgHeaderLength+len(payload))
+
 	message[0] = protocolID
 
 	binary.BigEndian.PutUint32(message[1:5], votingRoundID)
@@ -42,7 +49,7 @@ func BuildMessage(protocolID uint8, votingRoundID uint32, payload []byte) string
 //   - randomQualityScore (1 byte),
 //   - merkleRoot (32 bytes).
 func BuildMessageForSigning(protocolID uint8, roundID uint32, isSecureRandom bool, merkleRoot common.Hash) string {
-	data := make([]byte, 38)
+	data := make([]byte, submitSignatureHeaderLength, submitSignatureMsgLength)
 
 	data[0] = protocolID
 	binary.BigEndian.PutUint32(data[1:5], roundID)
@@ -52,7 +59,8 @@ func BuildMessageForSigning(protocolID uint8, roundID uint32, isSecureRandom boo
 	} else {
 		data[5] = 0
 	}
-	copy(data[6:38], merkleRoot[:])
+
+	data = append(data, merkleRoot[:]...)
 
 	return "0x" + hex.EncodeToString(data)
 }
