5c71727a66cf795bc8bcf9ae9278bcc5a0750021
Gregor Podlogar <gregor.podlogar@flare.network>
2026-04-20 15:18:42 +0200

    chore(logger): updated logger.Nop with more methods

--- pkg/logger/nop.go
+++ pkg/logger/nop.go
@@ -4,9 +4,20 @@ package logger
 // whose methods are a subset of the ones defined here.
 type Nop struct{}
 
-func (Nop) Debugf(_ string, _ ...any) {}
 func (Nop) Debug(_ ...any)            {}
-func (Nop) Infof(_ string, _ ...any)  {}
-func (Nop) Info(_ ...any)             {}
-func (Nop) Warnf(_ string, _ ...any)  {}
+func (Nop) Debugf(_ string, _ ...any) {}
+
+func (Nop) Info(_ ...any)            {}
+func (Nop) Infof(_ string, _ ...any) {}
+
+func (Nop) Warn(_ ...any)            {}
+func (Nop) Warnf(_ string, _ ...any) {}
+
+func (Nop) Error(_ ...any)            {}
 func (Nop) Errorf(_ string, _ ...any) {}
+
+func (Nop) Panic(args ...any)         {}
+func (Nop) Panicf(_ string, _ ...any) {}
+
+func (Nop) Fatal(args ...any)         {}
+func (Nop) Fatalf(_ string, _ ...any) {}
